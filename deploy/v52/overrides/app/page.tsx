716d6b612b5c6f195a927274b83f8a9d772880f0
vitaliyozolin-dotcom <vitaliyozolin@gmail.com>
2026-08-26 19:30:51 +0000

    fix: remove hardcoded owner name from application entry

--- deploy/v52/overrides/app/page.tsx
+++ deploy/v52/overrides/app/page.tsx
@@ -1,4 +1,4 @@
-import ArtHelloShell from "./components/ArtHelloShell";
+import AuthenticatedArtHelloShell from "./components/AuthenticatedArtHelloShell";
 import { ContextualHelpSystem } from "./components/ContextualHelpSystem";
 import ProductionAuthGate from "./components/ProductionAuthGate";
 
@@ -7,7 +7,7 @@ export const dynamic = "force-dynamic";
 export default function Home() {
   return (
     <ProductionAuthGate>
-      <ArtHelloShell displayName="Виталий Озолин" />
+      <AuthenticatedArtHelloShell />
       <ContextualHelpSystem />
     </ProductionAuthGate>
   );
