2080a825abc2301689edf98fe95d0718413579be
vitaliyozolin-dotcom <vitaliyozolin@gmail.com>
2026-09-11 20:59:05 +0300

    D148: remove obsolete ArtHello Pay rollback alias

--- lib/db/rollbacks/0019_arthello_pay_core.down.sql
+++
@@ -1,7 +0,0 @@
--- Rollback companion for 0019_arthello_pay_core.sql.
--- This removes only additive ArtHello Pay tables and does not mutate existing finance data.
-DROP TABLE IF EXISTS "fiscal_receipts";
-DROP TABLE IF EXISTS "payment_events";
-DROP TABLE IF EXISTS "payment_requests";
-DROP TABLE IF EXISTS "payment_obligations";
-DROP TABLE IF EXISTS "payment_routes";
