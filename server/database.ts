09b0a1653bf374a57e2bc04ff0f950cc027ec7c7
vitaliyozolin-dotcom <vitaliyozolin@gmail.com>
2026-09-15 13:06:41 -0700

    feat(diary): receive signed directory snapshots separately from access grants

--- server/database.ts
+++ server/database.ts
@@ -100,6 +100,7 @@ export function loadMigrationSql() {
     "0007_curriculum_import.sql",
     "0008_schedule_groups.sql",
     "0009_atlas_school.sql",
+    "0010_central_directory.sql",
   ];
   return filenames.map((filename) =>
     readFileSync(join(process.cwd(), "drizzle", filename), "utf8"),
